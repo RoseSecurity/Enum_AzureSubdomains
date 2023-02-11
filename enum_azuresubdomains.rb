16673f95c9e06010f5ed3b07ffd9bbf792bab7cd
RoseSecurity <72598486+RoseSecurity@users.noreply.github.com>
2023-02-10 19:38:50 -0500

    Update enum_azuresubdomains.rb

--- enum_azuresubdomains.rb
+++ enum_azuresubdomains.rb
@@ -20,7 +20,8 @@ class MetasploitModule < Msf::Auxiliary
       )
     register_options(
         [
-          OptString.new('DOMAIN', [true, 'The target domain without TLD (Ex: testorg rather than testorg.org)']),
+          OptString.new('DOMAIN', [true, 'The target domain without TLD (Ex: victim rather than victim.org)']),
+          OptBool.new('PERMUTATIONS', [false, 'Prepend and append permutated keywords to domain (This option can take minutes to complete)', false]),
           OptBool.new('ENUM_A', [true, 'Enumerate DNS A record', true]),
           OptBool.new('ENUM_CNAME', [true, 'Enumerate DNS CNAME record', true]),
           OptBool.new('ENUM_MX', [true, 'Enumerate DNS MX record', true]),
@@ -29,50 +30,127 @@ class MetasploitModule < Msf::Auxiliary
           OptBool.new('ENUM_TXT', [true, 'Enumerate DNS TXT record', true]),
         ])
     end
-  
-    def run
-        target = datastore['DOMAIN']
-        subdomains = [ "onmicrosoft.com", 
-        "scm.azurewebsites.net",
-        "azurewebsites.net",
-        "p.azurewebsites.net",
-        "cloudapp.net",
-        "file.core.windows.net",
-        "blob.core.windows.net",
-        "queue.core.windows.net",
-        "table.core.windows.net",
-        "mail.protection.outlook.com",
-        "sharepoint.com",
-        "redis.cache.windows.net",
-        "documents.azure.com",
-        "database.windows.net",
-        "vault.azure.net",
-        "azureedge.net",
-        "search.windows.net",
-        "azure-api.net",
-        "azurecr.io", ]
 
-        # Create array of target domains
-        targetdomains = Array.new()
+    def run
+      # Array of subdomains to enumerate
+      domain = datastore['DOMAIN']
+      subdomains = [ ".onmicrosoft.com", 
+          ".scm.azurewebsites.net",
+          ".azurewebsites.net",
+          ".p.azurewebsites.net",
+          ".cloudapp.net",
+          ".file.core.windows.net",
+          ".blob.core.windows.net",
+          ".queue.core.windows.net",
+          ".table.core.windows.net",
+          ".mail.protection.outlook.com",
+          ".sharepoint.com",
+          ".redis.cache.windows.net",
+          ".documents.azure.com",
+          ".database.windows.net",
+          ".vault.azure.net",
+          ".azureedge.net",
+          ".search.windows.net",
+          ".azure-api.net",
+          ".azurecr.io", ]
+
+      # Array of keywords to prepend and append
+      permutations = [ "root",
+          "web",
+          "api",
+          "azure",
+          "azure-logs",
+          "data",
+          "database",
+          "data-private",
+          "data-public",
+          "dev",
+          "development",
+          "demo",
+          "files",
+          "filestorage",
+          "internal",
+          "keys",
+          "logs",
+          "private",
+          "prod",
+          "production",
+          "public",
+          "service",
+          "services",
+          "splunk",
+          "sql",
+          "staging",
+          "storage",
+          "storageaccount",
+          "test",
+          "useast",
+          "useast2",
+          "centralus",
+          "northcentralus",
+          "westcentralus",
+          "westus",
+          "westus2", ]
+
+      # Create permutated array of keywords and target domain
+      if datastore['PERMUTATIONS']
+        permutated_domains = Array.new()
+        for keywords in permutations do
+            permutated_domains.append(domain + "-" + keywords)
+            permutated_domains.append(keywords + "-" + domain)
+        end
+
+        # Normal list of subdomains
+        target_domains = Array.new()
         for tld in subdomains do
-          targetdomains.append(target + "." + tld)
+          target_domains.append(domain + tld)
         end
-        
-        # Perform DNS queries
-        for domain in targetdomains do
+
+        # Permutated list of subdomains
+        for tld in subdomains do
+            for domain in permutated_domains do
+                target_domains.append(domain + tld)
+            end
+        end
+
+        # Query DNS records of permutated and normal target subdomains
+        for domain in target_domains do
           begin
-          if Resolv.getaddress domain
-            puts "\nDiscovered Target Domain: #{domain}"
-            dns_get_a(domain) if datastore['ENUM_A']
-            dns_get_cname(domain) if datastore['ENUM_CNAME']
-            dns_get_ns(domain) if datastore['ENUM_NS']
-            dns_get_mx(domain) if datastore['ENUM_MX']
-            dns_get_soa(domain) if datastore['ENUM_SOA']
-            dns_get_txt(domain) if datastore['ENUM_TXT']
+            if Resolv.getaddress domain
+              puts "\nDiscovered Target Domain: #{domain} \n"
+              dns_get_a(domain) if datastore['ENUM_A']
+              dns_get_cname(domain) if datastore['ENUM_CNAME']
+              dns_get_ns(domain) if datastore['ENUM_NS']
+              dns_get_mx(domain) if datastore['ENUM_MX']
+              dns_get_soa(domain) if datastore['ENUM_SOA']
+              dns_get_txt(domain) if datastore['ENUM_TXT']
+            end
+            rescue
+              next
+            end
           end
-          rescue
-            next
+      else
+
+        # Query DNS records of normal target subdomains
+        target_domains = Array.new()
+        for tld in subdomains do
+            target_domains.append(domain + tld)
+        end
+        for domain in target_domains do
+          begin
+            if Resolv.getaddress domain
+              puts "\nDiscovered Target Domain: #{domain} \n"
+              dns_get_a(domain) if datastore['ENUM_A']
+              dns_get_cname(domain) if datastore['ENUM_CNAME']
+              dns_get_ns(domain) if datastore['ENUM_NS']
+              dns_get_mx(domain) if datastore['ENUM_MX']
+              dns_get_soa(domain) if datastore['ENUM_SOA']
+              dns_get_txt(domain) if datastore['ENUM_TXT']
+            end
+            rescue
+              next
+            end
           end
         end
+      end
     end
-  end
